d88b103b7001f6885bd778b8ec302d7be1235770
Eduardo Lowensohn <gunchosoft@gmail.com>
2018-10-16 15:35:16 -0400

    Search

--- models/address_book.rb
+++ models/address_book.rb
@@ -55,10 +55,69 @@ class AddressBook
     end
         end
 
+        def Iterative_search(name)
+          @entries.each do |entry|
+            if entry.name == name
+              return entry
+              
+            end
+          end
+          return nil
+        end
+
+## Search AddressBook for a specific entry by name
+def binary_search(name)
+  ##1
+  lower = 0
+  upper = entries.length - 1
+
+  ##2
+
+  while lower <= upper
+    ##3
+    mid = (lower + upper) / 2
+    mid_name  =  entries[mid].name
+
+    ##4
+
+    if name == mid_name
+      return entries[mid]
+    elsif name < mid_name
+      upper = mid - 1
+    elsif name > mid_name
+      lower = mid + 1
+    end
+  end
+  ##5
+  return nil
+
+
+
+
+end
+
     end
 
 
+##At #1, we save the index of the leftmost item in the array in a variable named
+##lower, and the index of rightmost item in the array in upper. If we think of the
+##array in terms of left-to-right where the leftmost item is the zeroth index and
+##the rightmost item is the entries.length-1 index.
 
+##At #2, we loop while our lower index is less than or equal to our upper index.
+
+##At #3, we find the middle index by taking the sum of lower and upper and dividing
+##it by two. Ruby will truncate any decimal numbers, so if upper is five and lower
+## is zero then mid will get set to two. Then we retrieve the name of the entry at
+## the middle index and store it in mid_name.
+
+##At #4, we compare the name that we are searching for, name, to the name of the middle
+## index, mid_name. We use the == operator when comparing the names which makes the search case sensitive
+##If name is equal to mid_name we've found the name we are looking for so we return the entry at index mid.
+##If name is alphabetically before mid_name, then we set upper to mid - 1 because the name must be in the lower half of the array.
+##If name is alphabetically after mid_name, then we set lower to mid + 1 because the name must be in the upper half of the array.
+
+##At #5, if we divide and conquer to the point where no match is found, we return nil.
 
 
 ##To keep entries nicely ordered we'll code add_entry to insert entries in lexicographical order
